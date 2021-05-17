634a51fcdbbaa6366865a87a7518d9fb8dda82d6
Ashok Kumar saini <askumar9672929010@gmail.com>
2021-05-18 01:42:39 +0530

    Update express1.js

--- express1.js
+++ express1.js
@@ -1,17 +1,11 @@
+
 var express = require('express');
 var app = express();
-
+var fs = require('fs');
 //make a dictionary for few sample roll number inputs 
 
-var coin_Data = {
-    "100" : 6,
-    "101" : 15,
-    "102" : 8,
-    "103" : 9,
-    "104" : 13,
-    "105" : 19,
-    "106" : 8
-}
+var data_raw = fs.readFileSync('coin_Data.json'); 
+var coin_Data = JSON.parse(data_raw);
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,21 +13,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-
-app.post('/submit-student-data', function (req, res) {
+app.post('/coins', function (req, res) {
     var roll = req.body.rollno ;
-    //const data = await response.json();
+    
     if(coin_Data[roll]){
         reply = {
-            coins : coin_Data[roll]
+            "coins" : coin_Data[roll]
         }
-    }
-    else{
-      reply = "Not Found the roll no" ;
+    } else{
+      reply = "Not Found the roll no in Dictionary" ;
     }
     res.send(reply);
 });
 
-var server = app.listen(5000, function () {
+var server = app.listen(8080, function () {
     console.log('Node server is running..');
 });
